9fc82b00faeb265109fe11a475f4e918815d91fa
Joey-Cerulli <cerullij@union.edu>
2026-02-10 15:19:22 -0500

    Added ledc init function

--- main/main.c
+++ main/main.c
@@ -16,9 +16,9 @@
 #define SpeedSelector ADC_CHANNEL_7                 //Potentiometer pin for setting wiper speed
 #define ADC_ATTEN ADC_ATTEN_DB_12                   //ADC Attenuation
 #define BITWIDTH ADC_BITWIDTH_12                    //ADC Bitwidth
-#define SHORT 1                                     //Short delay for intermittent wipers
-#define MEDIUM 3                                    //Medium delay for intermittent wipers
-#define LONG 5                                      //Long delay for intermittent wipers
+#define SHORT (1)                                   //Short delay for intermittent wipers
+#define MEDIUM (3)                                  //Medium delay for intermittent wipers
+#define LONG (5)                                    //Long delay for intermittent wipers
 #define LEDC_TIMER LEDC_TIMER_0                     //Sets the LEDC timer
 #define LEDC_MODE LEDC_LOW_SPEED_MODE               //Sets the LEDC speed mode
 #define LEDC_OUTPUT_IO (5)                          //Define the output pin for LEDC
@@ -248,4 +248,29 @@ void WiperHandler(int Mode) {
     } else if (Mode = 3) {
 
     }
+}
+
+void ledc_init(void)
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .timer_num        = LEDC_TIMER,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 50 Hz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .timer_sel      = LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+ledc_channel_config(&ledc_channel);
 }
